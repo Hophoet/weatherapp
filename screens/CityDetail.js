052bbec48523c2e33b8008ae3b560e8a04ab236b
hophoet <hophoet@gmail.com>
2021-04-17 14:10:02 +0000

    (components, styles) - style CityDetail screens component

--- screens/CityDetail.js
+++ screens/CityDetail.js
@@ -12,10 +12,24 @@ export default class CityItem extends React.Component{
 		let city = this.props.navigation.state.params.city
 		return (
 			<View style={styles.container}>
-				<Text style={styles.cityName}>{city && city.name}</Text>
-				<Text
-					onPress={() => {this.props.navigation.navigate('CityList')}}
-					> NAVIGATE BACK TO LIST </Text>
+				<View style={styles.mapContainer}>
+					<Text
+						onPress={() => {this.props.navigation.navigate('CityList')}}
+						> NAVIGATE BACK TO LIST </Text>
+				</View>
+				<View style={styles.cityInfosContainer}>
+					<View style={styles.column1}>	
+						<Text style={styles.cityName}>{city && city.name}</Text>
+						<Text>Clear Sky</Text>
+						<Text>Humidity: 70</Text>
+						<Text>Wind Speed: 5.52</Text>
+						<Text>Max Temp: 38°c</Text>
+						<Text>Min Temp: 27°c</Text>
+					</View> 
+					<View style={styles.column2}>	
+						<Text style={styles.cityTemperature}> 25°c</Text>	
+					</View> 
+				</View>
 			</View>
 		)
 
@@ -27,12 +41,32 @@ export default class CityItem extends React.Component{
 const styles = StyleSheet.create({
 	container:{
 		flex:1,
+	},
+	mapContainer:{
+		flex:3,
+		backgroundColor:'gray',
+	},
+	cityInfosContainer:{
+		backgroundColor:'white',
+		flex:1,
+		flexDirection:'row',
+		padding:5
+	},
+	column1:{
+		//backgroundColor:'red',
+		flex:3
+	},
+	column2:{
+		//backgroundColor:'blue',
+		flex:1.5,
 		justifyContent:'center',
 		alignItems:'center'
 	},
 	cityName:{
-		fontSize:15,
-		fontWeight:'bold'
+		fontSize:20,
+	},
+	cityTemperature:{
+		fontSize:30
 	}
 
 })
