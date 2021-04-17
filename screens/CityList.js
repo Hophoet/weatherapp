8fa6b0cc71d07dda16a276b1c2712757ca15302e
hophoet <hophoet@gmail.com>
2021-04-17 14:55:34 +0000

    (api, methods, state) - add _getCities method to make the cities getting request base on getCities api function, update the cities data after the request

--- screens/CityList.js
+++ screens/CityList.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import {Text, View, FlatList, StyleSheet, TouchableOpacity} from 'react-native';
 import CityItem from '../components/CityItem';
+import {getCities} from '../api/functions';
 
 export default class CityList extends React.Component {
 	constructor(props){
 		super(props)
 		this.state = {
-			cities: [
-				{name:'bust'},
-				{name:'united state'},
-			]
+			cities: []
 		}
 	}
 
 	navigateToCityDetail = (data) => {
 		this.props.navigation.navigate('CityDetail', {city:data})
+	}
+
+	_getCities = () => {
+		getCities()
+		.then(response => {
+			console.log('response')
+			this.setState({cities:response.list});
+		})
+		.catch(error => {
+			console.log('error')
+			console.log(error);
+		})
+	}
+
+	componentDidMount(){
+		this._getCities();
 	}
 
 	render(){
