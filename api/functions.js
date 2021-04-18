826199218a3a8216223c21bba508908ae01cbdd1
hophoet <hophoet@gmail.com>
2021-04-18 07:31:05 +0000

    (api, functions) - set getTemp request function to get the user temperature with the user location

--- api/functions.js
+++ api/functions.js
@@ -1,4 +1,4 @@
-import {API_URL} from './config';
+import {API_URL, API_KEY} from './config';
 
 function getCities() {
 	return new Promise( (resolve, reject) => {
@@ -22,7 +22,28 @@ var 	myHeaders = new Headers();
 }
 
 
+function getTemp(lat, lon) {
+	return new Promise( (resolve, reject) => {
+ 	    var myHeaders = new Headers();
+		var requestOptions = {
+		  method: 'GET',
+		  headers: myHeaders,
+		};
+		let URL = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}` 
+		fetch(URL, requestOptions)
+		.then(response => {
+			return response.json();
+		})
+		.then(response => {
+			resolve(response);
+		})
+		.catch(error => {
+			reject(error);
+		})
+	})
+}
 
 export {
-	getCities
+	getCities,
+	getTemp
 }
