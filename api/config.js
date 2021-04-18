440565a02137fe03eb5ebf3dfa6d49f1e86144e3
hophoet <hophoet@gmail.com>
2021-04-18 07:30:19 +0000

    (api) - export the API_KEY

--- api/config.js
+++ api/config.js
@@ -2,5 +2,6 @@ const API_KEY = 'd337878c9d32adb1668606fa56bb2975'
 const API_URL = `http://api.openweathermap.org/data/2.5/find?lat=23.68&lon=90.35&cnt=50&appid=${API_KEY}`
 
 export {
-	API_URL
+	API_URL,
+	API_KEY
 }
