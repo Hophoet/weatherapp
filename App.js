a5a47b7c9d4ef4f9dd5b4fd68dc08d94e7c444e0
hophoet <hophoet@gmail.com>
2021-04-18 07:08:05 +0000

    (components, redux) - set the main app into the reducer Provider and PersistGate component to allow the global state management and it persistence

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import type {Node} from 'react';
+import {Provider} from 'react-redux'
+import {PersistGate} from 'redux-persist/integration/react'
+import {persistStore} from 'redux-persist'
+import Store from './redux/store/storeConfigurations'
+             
+
 import {
   Text,
   View,
@@ -10,8 +16,14 @@ import Nav from './navigations/Main';
 
 const App: () => Node = () => {
 
+  let persistor = persistStore(Store)
   return (
-	<Nav/>
+	   <Provider store={Store}>
+			<PersistGate persistor={persistor}>
+				<Nav/>
+			</PersistGate>
+       </Provider>
+
   );
 };
 
